f237b6e4c76de490393d742af83c544f5d59f177
edanur <kutluayedanur@gmail.com>
2023-01-27 10:14:02 +0300

    Menu Ekleme

--- Cafe/Models/Menu.cs
+++ Cafe/Models/Menu.cs
@@ -5,17 +5,17 @@ namespace Cafe.Models
 {
     public class Menu
     {
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        public string Title { get; set; }
-        [Required]
-        public string Description { get; set; }
-        public string Image { get; set; }
-        public bool Ozel { get; set; }
-        public double Price { get; set; }
-        public int CategoryId { get; set; }
-        [ForeignKey("CategoryId")]
-        public Category Category { get; set; }
-    }
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string Title { get; set; }
+		[Required]
+		public string Description { get; set; }
+		public string Image { get; set; }
+		public bool Ozel { get; set; }
+		public double Price { get; set; }
+		public int CategoryId { get; set; }
+		[ForeignKey("CategoryId")]
+		public Category Category { get; set; }
+	}
 }
